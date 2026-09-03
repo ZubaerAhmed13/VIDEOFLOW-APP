d0f49672ced64e7ea18216f1cd4fa779e8f0629d
Zubaer Ahmed <zubaerknight@gmail.com>
2026-09-03 17:09:07 +0200

    Fix Step 2 editor scroll-state import

--- app/src/main/java/com/videoflow/app/ui/screens/EditorScreen.kt
+++ app/src/main/java/com/videoflow/app/ui/screens/EditorScreen.kt
@@ -2,6 +2,7 @@ package com.videoflow.app.ui.screens
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -34,7 +35,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberScrollState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
